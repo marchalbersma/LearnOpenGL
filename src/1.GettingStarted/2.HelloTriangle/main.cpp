c728f211bcf84de3b12cf4365acae79bb8c81fd2
marchalbersma <contact@marchalbersma.com>
2022-07-21 23:48:58 +0200

    Create VBO and VAO.

--- src/1.GettingStarted/2.HelloTriangle/main.cpp
+++ src/1.GettingStarted/2.HelloTriangle/main.cpp
@@ -9,6 +9,20 @@ int main()
 
     Glad::init();
 
+    const float vertices[] = {
+        -0.33f, -0.5f,  0.0f,
+        0.33f,  -0.5f,  0.0f,
+        0.0f,   0.5f,   0.0f
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     GLFW::loop(window, []() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
