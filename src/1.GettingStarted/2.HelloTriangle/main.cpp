5ca41e83cbb3ca9d87e28f8e0c5a70b3515f15de
marchalbersma <contact@marchalbersma.com>
2022-07-22 00:07:38 +0200

    Render triangle.

--- src/1.GettingStarted/2.HelloTriangle/main.cpp
+++ src/1.GettingStarted/2.HelloTriangle/main.cpp
@@ -29,8 +29,12 @@ int main()
 
     Shader shader("shader.vert", "shader.frag");
 
-    GLFW::loop(window, []() {
+    GLFW::loop(window, [VAO, shader]() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     });
 }
