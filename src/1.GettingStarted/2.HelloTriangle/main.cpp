e422b45ca1c6953d8c843dd8728f75590421ed42
marchalbersma <contact@marchalbersma.com>
2022-07-22 00:05:38 +0200

    Link vertex attributes.

--- src/1.GettingStarted/2.HelloTriangle/main.cpp
+++ src/1.GettingStarted/2.HelloTriangle/main.cpp
@@ -24,6 +24,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
+    glEnableVertexAttribArray(0);
+
     Shader shader("shader.vert", "shader.frag");
 
     GLFW::loop(window, []() {
