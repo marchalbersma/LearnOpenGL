efc6464361a06aba627cb4eceec51883e2ddcb46
marchalbersma <contact@marchalbersma.com>
2022-07-22 10:44:30 +0200

    Add 1.GettingStarted/3.Shaders/main.cpp.

---
+++ src/1.GettingStarted/3.Shaders/main.cpp
@@ -0,0 +1,36 @@
+#include <glad/glad.h>
+#include <Glad.h>
+#include <GLFW.h>
+
+int main()
+{
+    GLFW::init();
+    GLFWwindow* window = GLFW::createWindow(1280, 720);
+
+    Glad::init();
+
+    const float vertices[] = {
+        -0.33f, -0.5f,  0.0f,
+        0.33f,  -0.5f,  0.0f,
+        0.0f,   0.5f,   0.0f
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
+    glEnableVertexAttribArray(0);
+
+    GLFW::loop(window, [VAO]() {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    });
+}
