10747acf7a87e640e78dd48b39e081e789147e01
marchalbersma <contact@marchalbersma.com>
2022-07-24 20:25:11 +0200

    Add 1.GettingStarted/4.Textures/main.cpp.

---
+++ src/1.GettingStarted/4.Textures/main.cpp
@@ -0,0 +1,39 @@
+#include <glad/glad.h>
+#include <Glad.hpp>
+#include <GLFW.hpp>
+#include <Vertex.hpp>
+
+using namespace glm;
+
+int main()
+{
+    GLFW::init();
+    GLFWwindow* window = GLFW::createWindow(1280, 720);
+
+    Glad::init();
+
+    const Vertex vertices[] {
+        Vertex { vec3(0.33f, -0.5f, 0.0f) },
+        Vertex { vec3(-0.33f, -0.5f, 0.0f) },
+        Vertex { vec3(0.0f, 0.5f, 0.0f) }
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position.x));
+    glEnableVertexAttribArray(0);
+
+    GLFW::loop(window, [VAO]() {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    });
+}
