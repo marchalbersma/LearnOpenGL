fd03dea231b627221617cffca05df1e2ddb1b3a9
marchalbersma <contact@marchalbersma.com>
2022-07-27 23:18:07 +0200

    Remove namespace from 'uniformName' parameter in Shader::setInt().

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -40,7 +40,7 @@ void Shader::setFloat(const string& uniformName, float value) const
     glUniform1f(uniforms.at(uniformName), value);
 }
 
-void Shader::setInt(const std::string& uniformName, int value) const
+void Shader::setInt(const string& uniformName, int value) const
 {
     glUniform1i(uniforms.at(uniformName), value);
 }
