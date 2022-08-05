0df71bf46dacf1edae448ac65c1c79142e46acfb
marchalbersma <contact@marchalbersma.com>
2022-08-05 14:32:14 +0200

    Add 3.ModelLoading/1.Model/main.cpp.

---
+++ src/3.ModelLoading/1.Model/main.cpp
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <Glad.hpp>
+#include <GLFW.hpp>
+
+int main()
+{
+    GLFW::init();
+    GLFWwindow* window = GLFW::createWindow(1280, 720);
+
+    Glad::init();
+
+    GLFW::loop(window, [&](float time, float deltaTime) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    });
+}
